12dd85bfcee67b67051a4d6a2e4e6d56b557e768
m.baturin <m.baturin@bars.group>
2024-02-26 20:36:41 +0300

    feat: init - добавлен пример реализации Npgsql+Dapper в ASP.NET Core приложении

---
+++ Database.AspNetCoreExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using Database.AspNetCoreExample.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Генерация документации Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Регистрация источника для соединений с БД.
+var connectionString = builder.Configuration.GetConnectionString("default")!;
+builder.Services.AddNpgsqlDataSource(connectionString);
+builder.Services.AddSingleton<IUserService, DapperUserService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+await app.RunAsync();
